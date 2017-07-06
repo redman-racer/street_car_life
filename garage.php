34b7e51453929656fefcf740dc10761e7d9912e6
carl.karlstrom <ababmxking@gmail.com>
2017-07-06 11:04:42 -0500

    fixed .css links

--- garage.php
+++ garage.php
@@ -5,7 +5,7 @@ require 'models/globals.php';
 <head>
 <title>The Street Car Life Game</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link href="layout/index.css?v=<?=time();?>" rel="stylesheet" type="text/css">
+<link href="layout/base.css?v=<?=time();?>" rel="stylesheet" type="text/css">
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 </head>
