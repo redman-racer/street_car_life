0323ebd76f0dca4b44d56e04077d467ed3b088eb
carl.karlstrom <ababmxking@gmail.com>
2017-07-07 18:30:58 -0500

    worked on garage a little more

--- app/views/garage.php
+++ app/views/garage.php
@@ -21,9 +21,11 @@ header("Refresh: $sec; url='garage'");
 
       //loop through the results and display them
       while($garage_row = $garage_stmt->fetch()) {
+
         if($garage_row['driving']){
 					$selected_display   = "";
           $ic_container_class = "ic_container_driving";
+					$driving_id         = $garage_row['base_id'];
         }else{
           $ic_container_class = "ic_container";
 					$selected_display   = "display: none;";
@@ -54,8 +56,16 @@ header("Refresh: $sec; url='garage'");
     </div>
 		<!--END car select thumbnail div-->
 		<!--BEGIN selected car display div - the large right div that displays the vehicles information-->
+		<?php
+		//select base_car for year,make,model by base_id stored in users_cars
+		$base_selected_stmt = $conn->prepare("SELECT * FROM  `base_cars` WHERE  `id`=:base_id");
+		$base_selected_stmt->bindParam(':base_id', $driving_id, PDO::PARAM_STR);
+		$base_selected_stmt->execute();
+		$base_selected = $base_selected_stmt->fetch();
+		?>
 		<div id="cd_container">
-
+					<div id="car_container"><img src="<?php echo $IMAGE_ROOT; ?>cars/garage/<?= $base_selected['photo_folder']; ?>/street-car-life-<?= $base_selected['year']; ?>-<?= $base_selected['make']; ?>-<?= $base_selected['model']; ?>-large-front.png" /></div>
+					<div id="car_stats" style="width: 400; height: 470px; border: 1px solid #000; position:absolute; top: 100px; left: 650px;"> </div>
 		</div>
 		<!--END selected car display div-->
 	</div>
