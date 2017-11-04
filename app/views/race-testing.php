9bd47b0c92ac466e3693c44133878fa5e73acd61
carl.karlstrom <ababmxking@gmail.com>
2017-11-04 12:52:27 -0500

    Race working

--- app/views/race-testing.php
+++ app/views/race-testing.php
@@ -42,38 +42,50 @@ require '../config/globals.php';
 		</div>
 	</div>
 </div>
-<div div="button_container" style="width:940px; height:200px;">
+<div id="button_container" style="width:940px; height:200px;">
 	<button id="lights">Turn Lights On</button>
 	<button id="start">Turn Engine On</button>
 	<button id="rev">Rev the Engine</button>
-	<button id="launch">Launch</button>
+	<button id="launch">Check Launch RPM</button>
+	<button id="race">Start Race</button>
 	<button id="idle">Idle</button>
 	<button id="off">Off</button>
 	<input type="number" id="launchRPM" placeholder="Set Launch RPM" />
 </div>
+<div id="start_lights_container"
+ 	style="
+										width:940px;
+										height:200px;
+										width: auto;
+										float: right;
+										position: absolute;
+										top: 100px;
+										left: 1000px;">
+	<button id="launchRaceButton" style="width: 125px; height: 125px; border-radius: 125px; background: darkgreen; font-weight: bold; color: white; font-size: 50px; display: none;">GO</button>
+	<div id="tree_container" style="width: 100%; height: 300px; background: #fff; border-radius: 3px; padding-top: 20px;">
+		<div id="stage1" style="background-color: orange; width: 60px; height: 60px; border: 1px solid #000; border-radius: 30px; margin: 5px auto; display: none;"> </div>
+		<div id="stage2" style="background-color: orange; width: 60px; height: 60px; border: 1px solid #000; border-radius: 30px; margin: 5px auto; display: none;"> </div>
+		<div id="amber_1" style="background-color: yellow; width: 30px; height: 30px; border: 1px solid #000; border-radius: 30px; margin: 5px auto; display: none;"> </div>
+		<div id="amber_2" style="background-color: yellow; width: 30px; height: 30px; border: 1px solid #000; border-radius: 30px; margin: 5px auto; display: none;"> </div>
+		<div id="amber_3" style="background-color: yellow; width: 30px; height: 30px; border: 1px solid #000; border-radius: 30px; margin: 5px auto; display: none;"> </div>
+		<div id="green" style="background-color: green; width: 30px; height: 30px; border: 1px solid #000; border-radius: 30px; margin: 5px auto; display: none;"> </div>
+		<div id="red" style="background-color: red; width: 30px; height: 30px; border: 1px solid #000; border-radius: 30px; margin: 5px auto; display: none;"> </div>
+	</div>
+</div>
+
 </body>
 <footer>
 <script src="<?php echo $JS_ROOT; ?>all.js"></script>
 <script>
-var lightsOn = false; var engOn = false; var holdRPMActive = false; var needleActive = false;
+var lightsOn = false; var engOn = false; var holdRPMActive = false; var needleActive = false; var greenLightOnTime = Date.now() * 1000;
 
 $("#lights").on( "click", function() {
 	if( lightsOn === true){
-		$('#speedo-container').fadeTo('slow', 0.2, function()
-		{
-			$(this).css("background-image", "url("+image_root+"race/tachometer-web-base-lights-out.jpg)");
-			$('#spedo_needle_img').css("opacity", .35);
-			lightsOn = false;
-		}).fadeTo('slow', 1);
+			turnLightsOff();
 		return;
 	}
-	
-	$('#speedo-container').fadeTo('slow', 0.2, function()
-	{
-	    $(this).css("background-image", "url("+image_root+"race/tachometer-web-base.jpg)");
-		$('#spedo_needle_img').css("opacity", 1);
-		lightsOn = true;
-	}).fadeTo('slow', 1);
+
+	turnLightsOn();
 });
 
 $("#start").on( "click", function() {
@@ -267,17 +279,146 @@ $("body").on("click", "#off", function() {
 	needleActive = false;
 	rotateImage(200, 200, 'spedo_needle_img');
 	setTimeout( function(){
+		turnLightsOff();
 		holdRPMActive = false;
 		needleActive = false;
 		engOn = false;
 	}, 300 );
+});
 
-	$('#speedo-container').fadeTo('slow', 0.2, function()
-	{
-		$(this).css("background-image", "url("+image_root+"race/tachometer-web-base-lights-out.jpg)");
-		$('#spedo_needle_img').css("opacity", .35);
-		lightsOn = false;
-	}).fadeTo('slow', 1);
+$("body").on("click", "#race", function() {
+	// Set the time that the race was initiated
+	createdTime=Date.now();
+	//Start the checks that could stop the function
+	// Check to see if another animation is playing
+	if(needleActive === true){
+		alert("Please wait for the current animation to finish first.");
+		return;
+	}
+	// Check to see if the engine is on
+	if( engOn == false ){
+		alert("You must start your engine first.");
+		return;
+	}
+	// Check to see if launch RPM is set below Idle rpm
+	if (launchRPM <= 799 ){
+		alert("You must enter a number greater than the idle speed of 800rpm.");
+		return;
+	}
+	// Check to see if Launch RPM is greater than redline
+	if (launchRPM >= 7501 ){
+		alert("You can not launch the car higher than the rev limiter.");
+		return;
+	}
+
+
+		//Get the launch RPM
+		launchRPM  = $("#launchRPM").val();
+		//Find what RPM the needle is at now
+		currentRPM = (($("#spedo_needle_img").getRotateAngle() + 6)*41.66);
+		//Get the difference bewtween Launch RPM and the current RPM to make the animation
+		rpmDiff    = launchRPM - currentRPM;
+		// Set the length of time the rev take to get to the launchRPM (10milisecs for every 1 rpm)
+		rpmDiffTime = rpmDiff/10;
+		// Stop the idle or holdRPM animation
+		holdRPMActive = false;
+		delay = 1;//TODO remove or make this var work
+
+			//Turn the launch button on
+			setTimeout( function(){
+				$("#launchRaceButton").fadeIn(200);
+			});
+			//Rev engine to Launch RPM
+			setTimeout( function(){
+				rotateImage(launchRPM, rpmDiffTime, 'spedo_needle_img');
+			}, 140 );
+			//Hold RPM at launch
+			setTimeout( function(){
+				// Initiate idle or holdRPM animation
+				holdRPMActive = true;
+				holdRPM(launchRPM);
+			}, rpmDiffTime );
+			// Light the pre-stage bulbs
+			setTimeout(stage1, rpmDiffTime);
+			setTimeout(stage2, rpmDiffTime+1840);
+
+		// Start lighting the christmas tree
+		setTimeout(amber1, rpmDiffTime+2840);
+		setTimeout(amber2, rpmDiffTime+3340);
+		setTimeout(amber3, rpmDiffTime+3840);
+		setTimeout(green, rpmDiffTime+4340);
+		function stage1 (){
+			delay = delay - 1000; // if the player redlights, this var delays the activation of the startRace to properly set the R/T.
+			$("#stage1").toggle();
+		}
+		function stage2 (){
+			delay = delay - 1000; // if the player redlights, this var delays the activation of the startRace to properly set the R/T.
+			$("#stage2").toggle();
+		}
+		function amber1 (){
+			delay = delay - 1000; // if the player redlights, this var delays the activation of the startRace to properly set the R/T.
+			$("#amber_1").toggle();
+		}
+		function amber2 (){
+			delay = delay - 500;
+			$("#amber_2").toggle();
+		}
+		function amber3 (){
+			delay = delay - 500;
+			$("#amber_3").toggle();
+		}
+		function green (){
+			delay = delay - 500;
+			$("#green").toggle();
+			greenLightOnTime=Date.now(); // Set the time that the green light was lit, to compare player RT
+		}
+});
+
+
+$("body").on("click", "#launchRaceButton", function() {
+		// Check to see if play red lit
+		playerLaunchedTime=Date.now();
+		$(this).fadeOut(300);
+		
+		if( playerLaunchedTime < greenLightOnTime){
+			$("#red").fadeIn(300);
+		}
+
+		//Launch
+		dropRPM = launchRPM * .5; if (dropRPM <= 750) dropRPM = 800;
+
+			holdRPMActive = false;
+			rotateImage(dropRPM, 200, 'spedo_needle_img');
+		// First gear
+		setTimeout( function(){
+			rotateImage(7200, 2900, 'spedo_needle_img');
+		}, 200 );
+		// Second gear shift
+		setTimeout( function(){
+			rotateImage(4500, 180, 'spedo_needle_img');
+		}, 3100 );
+		// Second gear
+		setTimeout( function(){
+			rotateImage(7700, 2600, 'spedo_needle_img');
+		}, 3280 );
+		// Third gear shift
+		setTimeout( function(){
+			rotateImage(4800, 220, 'spedo_needle_img');
+		}, 5880 );
+		// Third Gear
+		setTimeout( function(){
+			rotateImage(7300, 2300, 'spedo_needle_img');
+		}, 6000 );
+		// Slow Down
+		setTimeout( function(){
+			rotateImage(800, 2500, 'spedo_needle_img');
+		}, 8300 );
+		// Start Idle
+		setTimeout( function(){
+			holdRPMActive = true;
+			holdRPM(600);
+		}, 10800 );
+	return true;
 });
 
 
@@ -322,14 +463,36 @@ function holdRPM(rpm){
 }
 
 function turnLightsOn(){
+	if (lightsOn === true){
+		alert("Your lights are already on!");
+		return false;
+	}
+
 	$('#speedo-container').fadeTo('slow', 0.2, function()
 	{
 		$(this).css("background-image", "url("+image_root+"race/tachometer-web-base.jpg)");
 		$('#spedo_needle_img').css("opacity", 1);
 		lightsOn = true;
 	}).fadeTo('slow', 1);
+
+	return true;
 }
 
+function turnLightsOff(){
+	if (lightsOn === false){
+		alert("Your lights are already off!");
+		return false;
+	}
+
+	$('#speedo-container').fadeTo('slow', 0.2, function()
+	{
+		$(this).css("background-image", "url("+image_root+"race/tachometer-web-base-lights-out.jpg)");
+		$('#spedo_needle_img').css("opacity", .35);
+		lightsOn = false;
+	}).fadeTo('slow', 1);
+
+	return true;
+}
 
 function rotateImage(rpm, duration, imgID){
 		startAng = $("#"+imgID).getRotateAngle();
